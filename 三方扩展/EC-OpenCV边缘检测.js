2af76ba57f2d86800f4e757a0e11c9ae83b6d390
cf183838 <751238382@qq.com>
2020-08-19 09:40:29 +0800

    OpenCV函数封装
    更新文件

--- 三方扩展/EC-OpenCV边缘检测.js
+++ 三方扩展/EC-OpenCV边缘检测.js
@@ -1,8 +1,8 @@
 /**
  * 参数1:待执行边缘检测的图片,类型 Mat
- * 参数2:待输出的图片,类型Mat
- * 参数3:二值化的下界
- * 参数4:二值化的上界
+ * 参数2:二值化的下界
+ * 参数3:二值化的上界
+ * 返回值:待输出的图片,类型 Mat
  * @Param Canny 中文Canny边缘算子
  */
 function Canny(image,threshold1,threshold2){
@@ -11,5 +11,17 @@ function Canny(image,threshold1,threshold2){
     importPackage(org.opencv.imgproc)
     var ret = new Mat();
     Imgproc.Canny(image,ret,threshold1,threshold2)
+    return ret
+}
+
+/**
+ * 参数1:文件名(含路径),类型 String
+ * 返回值:待输出的图片,类型 Mat
+ * @Param Imread 中文读取图片
+ */
+
+function Imread(filename){
+    let ret = new Mat()
+    ret = Imgcodecs.imread(filename)
     return ret
 }
